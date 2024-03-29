1aa27e367002aaa0651d12635a53e1a8a5bc8a87
Jeanderson1981 <163357736+Jeanderson1981@users.noreply.github.com>
2024-03-29 11:43:07 -0300

    Add files via upload
    Aula Online 28.03.2024

---
+++ funcoes.js
@@ -0,0 +1,91 @@
+function minhaFun (variavel){
+
+return variavel *5
+
+}
+
+console.log (minhaFun(2))
+console.log ( minhaFun(10))
+/* respostas 
+ a) vai aprecer impresso 10 e 50
+ b) nao vai imprimir nada*/
+
+let textoDoUsuario = ("Fruteira")
+let outraFuncao = function (texto){
+
+
+    return texto.toLowerCase (textoDoUsuario).includes ( "cenoura" )
+     /* funcao coloca todas letras minusculas*/
+}
+ const resposta = outraFuncao (textoDoUsuario)
+
+ console.log (resposta)
+
+ /* Exercicios de Escrita */
+
+ function nada ( ) {
+
+ const alterar = "brasileiro, gaucho tricolor"
+ return nada ("Jean estudante senac noite, tenho 43 anos")
+ }
+console.log (nada)
+
+let name = "Ana"
+let profissao = "cabelereira"
+let idade = 25
+let cidade = "Esteio"
+
+function unificar ( name, profissao, idade , cidade ){
+    console.log (unificar)
+    
+}
+
+console.log (unificar)
+
+
+function doisNumeros ( soma ){
+let numer= 20
+let numero= 15
+let soma = numer + numero
+
+}
+function nova ( numer >= numero) {
+}
+
+console.log (doisNumeros)
+let saberClassif = ((numer) / 2) {
+if 
+
+else
+}
+console.log (saberClassif){
+
+}
+
+let mensagem = "Senac noite Sao Leo"
+
+    console.log (mensagem.toUppercase())
+
+    
+let soma = 10
+let mult = 4
+let divide = 2
+let subtracao = 1
+let numInserido = 30
+let numInsert = 3
+
+{
+function one ( numInserido, soma)
+function two (numInsert , divide)
+function four ( numInserido , subtracao)
+function five (numInsert , mult)
+
+}
+console.log (one) 
+console.log (two) 
+console.log (four) 
+console.log (five)
+
+
+
+
